bc962f422d59a4e52a54ae546183bd3b35a937b0
13imanishi <fukutani@saga-lab.org>
2024-09-27 20:25:53 +0900

    changecolor
    試しでボタンの色ランダムで変えます

---
+++ DVA/Assets/ColorChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    GameObject[] button;
+    public int number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GameObject.FindGameObjectsWithTag("sphere");
+        number = Random.Range(0,3);
+        button[number].GetComponent<Renderer>().material.color = Color.red;
+        Debug.Log(button[0]);
+        Debug.Log(number);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
