eff468f4ecf8f28ad3253f94dade2a0ec9fd187b
13imanishi <fukutani@saga-lab.org>
2024-09-28 00:23:08 +0900

    制限時間表示，テスト用Hand
    制限時間を表示，テキストの場所を移動
    VRなくてもテストできるように操作できるBoxを実装，でも衝突できてないからテストできない
    ボタンの色を変えるのをコルーチンでする予定
    時間経過か押せたボタンの数によって同時に光る数を増やす感じで

--- DVA/Assets/ColorChange.cs
+++ DVA/Assets/ColorChange.cs
@@ -6,19 +6,25 @@ public class ColorChange : MonoBehaviour
 {
     GameObject[] button;
     public int number;
+
     // Start is called before the first frame update
     void Start()
     {
         button = GameObject.FindGameObjectsWithTag("sphere");
-        number = Random.Range(0,4);
-        button[number].GetComponent<Renderer>().material.color = Color.red;
-        Debug.Log(button[0]);
-        Debug.Log(number);
+        StartCoroutine("ChangetheColor1");
+        // Debug.Log(button.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator ChangetheColor1 () //はじめの段階，1つボタンの色を変える
+    {
+        number = Random.Range(0,button.Length); //すべてのボタンからランダムで一つ選択
+        button[number].GetComponent<Renderer>().material.color = Color.red; //赤に変更
+        yield return new WaitForSeconds(2);
     }
 }
---
+++ DVA/Assets/timercount.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timercount : MonoBehaviour
+{
+    public float timer = 90.0f; // 別のスクリプトで使う（予定）
+    public Text TimeCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        TimeCounter.text = "残り時間 ： "+timer.ToString("n2"); //残り時間の表示
+
+        if(timer <= 0.0f)
+        {
+            TimeCounter.text = "残り時間 ： 0"; //0で止めてマイナスにいかないように
+        }
+    }
+}
