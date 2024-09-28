8d38049aee239545ab2f37d587b20f6fa6a59988
Ichimaru103 <Ichimaru@saga-lab.org>
2024-09-28 20:34:13 +0900

    ModifiedColorChange.cs
    ColorChangeを時間で色を変化させた
    同じボタンが2回連続で光らないようにした
    oculusのPluginの問題を解消した
    最後の得点を表示するsceneを作った

--- DVA/Assets/ColorChange.cs
+++ DVA/Assets/ColorChange.cs
@@ -1,24 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorChange : MonoBehaviour
 {
     GameObject[] button;
     public int number;
+    int before_number;
+    
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
         button = GameObject.FindGameObjectsWithTag("sphere");
-        number = Random.Range(0,4);
-        button[number].GetComponent<Renderer>().material.color = Color.red;
-        Debug.Log(button[0]);
-        Debug.Log(number);
+        timer = 0;
+        number = -1; // first time not change color
+        Debug.Log("lenght" + button.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        timer += Time.deltaTime;
+       
+        if(timer > 1.0f)
+        {
+            before_number = number;
+            if(before_number >= 0)
+            {
+                button[before_number].GetComponent<Renderer>().material.color = Color.black;
+            }
+           
+            number = Random.Range(0, button.Length);
+            
+            Debug.Log("before_number" + before_number);
+            
+            while (number == before_number)
+            {
+                number = Random.Range(0, 4);
+                
+            }
+            Debug.Log("number" + number);
+            Change();
+            timer = 0;
+        }
+
+
+
+    }
+
+    void Change()
+    {
+        
+        button[number].GetComponent<Renderer>().material.color = Color.red;
     }
 }
