d343c5aaffd673148bc147e98292f53c255a1ada
yamaguchi0302 <129931695+yamaguchi0302@users.noreply.github.com>
2024-09-28 22:47:15 +0900

    start display
    titleシーンを作成し、ボタンを押すことでDVAシーンに遷移するようにしました。また、時間経過でランダムに赤い球が変わるようにColorChange.csも変更しています。スフィアのマテリアルが変わっているので注意してください。

--- DVA/Assets/ColorChange.cs
+++ DVA/Assets/ColorChange.cs
@@ -6,19 +6,54 @@ public class ColorChange : MonoBehaviour
 {
     GameObject[] button;
     public int number;
+    int before_number;
+    [SerializeField] Material _material;
+
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
         button = GameObject.FindGameObjectsWithTag("sphere");
-        number = Random.Range(0,4);
-        button[number].GetComponent<Renderer>().material.color = Color.red;
-        Debug.Log(button[0]);
-        Debug.Log(number);
+        timer = 0;
+        number = -1; // first time not change color
+        Debug.Log("lenght" + button.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        timer += Time.deltaTime;
+
+        if (timer > 1.0f)
+        {
+            before_number = number;
+            if (before_number >= 0)
+            {
+                button[before_number].GetComponent<Renderer>().material = _material;
+            }
+
+            number = Random.Range(0, button.Length);
+
+            Debug.Log("before_number" + before_number);
+
+            while (number == before_number)
+            {
+                number = Random.Range(0, button.Length);
+
+            }
+            Debug.Log("number" + number);
+            Change();
+            timer = 0;
+        }
+
+
+
+    }
+
+    void Change()
+    {
+
+        button[number].GetComponent<Renderer>().material.color = Color.red;
     }
 }
